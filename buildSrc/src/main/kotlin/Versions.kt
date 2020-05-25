fd9ccf474e67b83dd1e01e1551c13307786c7515
Sergey Morgunov <hostage@me.com>
2020-05-25 23:14:07 +0300

    Support Scala 2.13

---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,18 @@
+@Suppress("ObjectPropertyName")
+object Versions {
+    const val kotlin = "1.3.72"
+    const val scalaBinary = "2.13"
+    const val lagom = "1.6.2"
+    const val play = "2.8.1"
+    const val ktlint = "0.32.0"
+    const val `ktlint-plugin` = "8.0.0"
+    const val `kotlin-logging` = "1.6.22"
+    const val jacoco = "0.8.4"
+    const val junit5 = "5.3.2"
+    const val assertj = "3.11.1"
+    const val `kafka-junit5` = "3.1.0"
+    const val kafka = "2.4.1"
+    const val `nexus-staging` = "0.21.2"
+    const val `nexus-publish` = "0.4.0"
+    const val dokka = "0.10.1"
+}
