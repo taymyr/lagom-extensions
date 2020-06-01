f46c13365153982387b23939bea2cd2acc46f788
Sergey Morgunov <hostage@me.com>
2020-06-01 13:58:47 +0300

    Up version to '0.5.0-SNAPSHOT'

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ allprojects {
 
 subprojects {
     group = "org.taymyr.lagom"
-    version = "0.4.0-SNAPSHOT"
+    version = "0.5.0-SNAPSHOT"
 
     apply<JacocoPlugin>()
     apply<NexusPublishPlugin>()
