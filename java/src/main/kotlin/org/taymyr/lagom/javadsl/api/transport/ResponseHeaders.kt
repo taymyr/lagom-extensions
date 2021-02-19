58f8eebca326bb2132204d7def7143b499d1c000
atyutin <tutin.andrey@gmail.com>
2021-02-20 02:09:38 +0300

    Adds found service response for ResponseHeader.

--- java/src/main/kotlin/org/taymyr/lagom/javadsl/api/transport/ResponseHeaders.kt
+++ java/src/main/kotlin/org/taymyr/lagom/javadsl/api/transport/ResponseHeaders.kt
@@ -4,11 +4,14 @@ import akka.japi.Pair
 import com.lightbend.lagom.javadsl.api.transport.MessageProtocol
 import com.lightbend.lagom.javadsl.api.transport.ResponseHeader
 import com.lightbend.lagom.javadsl.api.transport.ResponseHeader.OK
+import org.pcollections.HashTreePMap
 import org.taymyr.lagom.javadsl.api.transport.MessageProtocols.JSON
 import play.core.cookie.encoding.Cookie
 import play.core.cookie.encoding.ServerCookieDecoder
 import play.core.cookie.encoding.ServerCookieEncoder
+import play.mvc.Http.HeaderNames.LOCATION
 import play.mvc.Http.HeaderNames.SET_COOKIE
+import play.mvc.Http.Status
 
 /**
  * Utilities and constants for [ResponseHeader].
@@ -17,6 +20,9 @@ object ResponseHeaders {
 
     @JvmField
     val OK_JSON = OK.withProtocol(JSON)!!
+
+    @JvmField
+    val FOUND = ResponseHeader(Status.FOUND, MessageProtocol(), HashTreePMap.empty())
 
     /**
      *
@@ -44,6 +50,18 @@ object ResponseHeaders {
     @JvmStatic
     fun <T> ok(protocol: MessageProtocol, data: T): Pair<ResponseHeader, T> {
         return Pair.create(OK.withProtocol(protocol), data)
+    }
+
+    /**
+     *
+     * Create moved temporarily service response with MIME-TYPE.
+     * @param T Type of data
+     * @param location location where moved
+     * @return Response with `302 FOUND` code and `Location` HTTP header
+     */
+    @JvmStatic
+    fun <T> found(location: String): Pair<ResponseHeader, T> {
+        return Pair.create(FOUND.withHeader(LOCATION, location), null)
     }
 
     /**
