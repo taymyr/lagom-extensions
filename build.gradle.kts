f18850fcfbe858d9b117954f07a269d73277e51d
Sergey Morgunov <hostage@me.com>
2021-02-26 19:58:51 +0300

    Release version '0.13.0'

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ allprojects {
 
 subprojects {
     group = "org.taymyr.lagom"
-    version = "0.13.0-SNAPSHOT"
+    version = "0.13.0"
 
     apply<JacocoPlugin>()
     apply<NexusPublishPlugin>()
