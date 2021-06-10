bd5e00828f468fcbd40bb66b9d09dbc011a78719
Sergey Morgunov <hostage@me.com>
2021-06-10 15:48:24 +0300

    Drop Kotlin Logging as a transitive dependency

--- java/src/main/kotlin/org/taymyr/lagom/javadsl/broker/SimpleTopicProducersRegistry.kt
+++ java/src/main/kotlin/org/taymyr/lagom/javadsl/broker/SimpleTopicProducersRegistry.kt
@@ -11,13 +11,11 @@ import com.lightbend.lagom.javadsl.api.broker.Topic
 import com.lightbend.lagom.javadsl.api.broker.kafka.KafkaProperties
 import com.lightbend.lagom.javadsl.api.deser.MessageSerializer.NegotiatedSerializer
 import com.typesafe.config.Config
-import mu.KotlinLogging
 import org.apache.kafka.common.serialization.Serializer
+import org.slf4j.LoggerFactory
 import java.util.concurrent.ConcurrentHashMap
 import javax.inject.Inject
 import javax.inject.Singleton
-
-private val log = KotlinLogging.logger {}
 
 /**
  * Registry of [SimpleTopicProducer].
@@ -43,24 +41,25 @@ class SimpleTopicProducersRegistry @Inject constructor(
         service.descriptor().topicCalls()
             .filter { tc -> topics.isEmpty() || topics.map { it.topicId() }.contains(tc.topicId()) }
             .forEach { topicCall ->
-            if (topicCall.topicHolder() is MethodTopicHolder) {
-                val topicId = topicCall.topicId()
-                producers[topicId.value()] = SimpleTopicProducer(
-                    serviceLocator,
-                    topicId,
-                    (topicCall as TopicCall<Any>).properties().getValueOf(KafkaProperties.partitionKeyStrategy()),
-                    TopicMessageSerializer(topicCall.messageSerializer().serializerForRequest()),
-                    materializer, actorSystem, config
-                )
-            } else {
-                log.error { """
+                if (topicCall.topicHolder() is MethodTopicHolder) {
+                    val topicId = topicCall.topicId()
+                    producers[topicId.value()] = SimpleTopicProducer(
+                        serviceLocator,
+                        topicId,
+                        (topicCall as TopicCall<Any>).properties().getValueOf(KafkaProperties.partitionKeyStrategy()),
+                        TopicMessageSerializer(topicCall.messageSerializer().serializerForRequest()),
+                        materializer, actorSystem, config
+                    )
+                } else {
+                    log.error(
+                        """
                     Can not register simple topic producer for topic ${topicCall.topicId()}.
                     Reason was that it was expected a topicHolder of type ${MethodTopicHolder::class.java.name}
                     but ${topicCall?.topicHolder()?.javaClass?.name} was found instead.
                     """.trimIndent()
+                    )
                 }
             }
-        }
         return this
     }
 
@@ -74,7 +73,8 @@ class SimpleTopicProducersRegistry @Inject constructor(
      */
     @Suppress("unchecked_cast")
     fun <M> get(topicDescriptor: TopicDescriptor<M>): SimpleTopicProducer<M> {
-        val p = producers[topicDescriptor.id] ?: throw IllegalArgumentException("Topic with name ${topicDescriptor.id} and record type ${topicDescriptor.type} is not registered.")
+        val p = producers[topicDescriptor.id]
+            ?: throw IllegalArgumentException("Topic with name ${topicDescriptor.id} and record type ${topicDescriptor.type} is not registered.")
         return p as SimpleTopicProducer<M>
     }
 
@@ -83,12 +83,17 @@ class SimpleTopicProducersRegistry @Inject constructor(
      *
      * @param M type of an entity being serialized
      */
-    private inner class TopicMessageSerializer<M> (private val serializer: NegotiatedSerializer<M, ByteString>) : Serializer<M> {
+    private inner class TopicMessageSerializer<M>(private val serializer: NegotiatedSerializer<M, ByteString>) :
+        Serializer<M> {
 
         override fun configure(configs: Map<String, *>, isKey: Boolean) {}
 
         override fun serialize(topic: String, data: M): ByteArray = serializer.serialize(data).toArray()
 
         override fun close() {}
+    }
+
+    companion object {
+        private val log = LoggerFactory.getLogger(SimpleTopicProducersRegistry::class.java)
     }
 }
