3c987323ca13a1d2ec0d488a79203b054cabaea2
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-19 14:53:31 +0800

    Delete buildSrc directory

--- buildSrc/src/main/kotlin/Variables.kt
+++
@@ -1,7 +0,0 @@
-import org.gradle.api.Project
-
-val Project.isRelease get() = !this.version.toString().endsWith("SNAPSHOT")
-
-val Project.lagomVersion get() = this.properties["lagomVersion"] as String? ?: Versions.lagom
-val Project.playVersion get() = this.properties["playVersion"] as String? ?: Versions.play
-val Project.scalaBinaryVersion get() = this.properties["scalaBinaryVersion"] as String? ?: Versions.scalaBinary
