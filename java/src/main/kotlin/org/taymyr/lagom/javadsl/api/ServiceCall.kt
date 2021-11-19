64279fe20189a983a75c03486fc541716707eeac
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-19 14:54:30 +0800

    Delete java directory

--- java/src/main/kotlin/org/taymyr/lagom/javadsl/api/ServiceCall.kt
+++
@@ -1,9 +0,0 @@
-package org.taymyr.lagom.javadsl.api
-
-import com.lightbend.lagom.javadsl.api.ServiceCall
-import kotlinx.coroutines.future.await
-
-/**
- * Helper extension method for [ServiceCall.invoke] call chain.
- */
-suspend fun <Request, Response> ServiceCall<Request, Response>.await(): Response = this.invoke().await()
